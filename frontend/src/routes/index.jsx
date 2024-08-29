8add1f283a9aaa217f52152a8543ca1f71fe9415
Sandipto Roy <140878093+sandipto729@users.noreply.github.com>
2024-08-29 23:36:14 +0530

    Update index.jsx

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,13 +1,13 @@
 // router.js
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import App from './../App'; // Make sure this path is correct
+import App from './../App';
 
-const router = createBrowserRouter([
+const routers = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <App/>,
   }
 ]);
 
-export default router;
+export default routers;
